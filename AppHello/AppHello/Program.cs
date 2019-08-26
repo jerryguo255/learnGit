94ad307301e347d33658b7f1a8aed10ef442ffd9
jerryguo255 <jerryguo255@outlook.com>
2019-08-26 15:17:36 +1200

    create a hello app

---
+++ AppHello/AppHello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
